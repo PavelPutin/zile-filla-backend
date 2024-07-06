04592938866408f5f01198edaa0ea0934a5d50fc
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-07-06 13:24:05 +0300

    test: add test for FileSystemUtilsTest getPathForRoot

---
+++ src/main/java/ru/vsu/pavel/zilefillabackend/util/FileSystemUtils.java
@@ -0,0 +1,19 @@
+package ru.vsu.pavel.zilefillabackend.util;
+
+import java.nio.file.Path;
+
+public class FileSystemUtils {
+    /**
+     * Получает путь в поддереве файловой системы, с корнем в директории root
+     * @param path путь внутри поддерева, начинающегося в root
+     * @param root корневая директория поддерева
+     * @return путь в поддереве
+     */
+    public static Path getPathForRoot(final Path path, final Path root) {
+        if (!root.isAbsolute()) {
+            throw new IllegalArgumentException("Root must be absolute");
+        }
+        var normalized = path.normalize();
+        return root.resolve(normalized);
+    }
+}
---
+++ src/test/java/ru/vsu/pavel/zilefillabackend/util/FileSystemUtilsTest.java
@@ -0,0 +1,27 @@
+package ru.vsu.pavel.zilefillabackend.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static ru.vsu.pavel.zilefillabackend.util.FileSystemUtils.getPathForRoot;
+
+class FileSystemUtilsTest {
+    @Test
+    public void givenPathAndAbsoluteRoot_thenReturnResolvedPath() {
+        Path path = Paths.get("ets\\photos.txt");
+        Path root = Paths.get("C:\\custom-root");
+        Path actual = getPathForRoot(path, root);
+        Path expected = Paths.get("C:\\custom-root\\ets\\photos.txt");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void givenPath_whenRootIsNotAbsolute_thenThrow() {
+        Path path = Paths.get("ets\\photos.txt");
+        Path root = Paths.get("custom-root");
+        assertThrows(IllegalArgumentException.class, () -> getPathForRoot(path, root));
+    }
+}
