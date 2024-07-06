994c8f60051551e9638a2aff8497ccc59f110c4d
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-07-06 12:02:47 +0300

    Initial commit

---
+++ src/main/java/ru/vsu/pavel/zilefillabackend/ZileFillaBackendApplication.java
@@ -0,0 +1,12 @@
+package ru.vsu.pavel.zilefillabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZileFillaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZileFillaBackendApplication.class, args);
+    }
+}
---
+++ src/test/java/ru/vsu/pavel/zilefillabackend/ZileFillaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsu.pavel.zilefillabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZileFillaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
