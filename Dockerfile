e88bdef4ff9168a15f46a4da1e20bffc40c8c74c
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-07-07 14:12:05 +0300

    fix: mock file system

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM gradle:jdk21 AS build
+COPY --chown=gradle:gradle . /home/gradle/src
+WORKDIR /home/gradle/src
+RUN gradle build --no-daemon
+
+FROM eclipse-temurin:21-jre
+EXPOSE 8080
+RUN mkdir /app
+COPY --from=build /home/gradle/src/build/libs/*.jar /app/spring-boot-application.jar
+ENTRYPOINT ["java", "-jar", "/app/spring-boot-application.jar"]
--- src/test/java/ru/vsu/pavel/zilefillabackend/util/FileSystemUtilsTest.java
+++ src/test/java/ru/vsu/pavel/zilefillabackend/util/FileSystemUtilsTest.java
@@ -1,7 +1,11 @@
 package ru.vsu.pavel.zilefillabackend.util;
 
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.FileSystem;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -11,17 +15,25 @@ import static ru.vsu.pavel.zilefillabackend.util.FileSystemUtils.*;
 class FileSystemUtilsTest {
     @Test
     public void givenPathAndAbsoluteRoot_thenReturnResolvedPath() {
-        Path path = Paths.get("ets\\photos.txt");
-        Path root = Paths.get("C:\\custom-root");
-        Path actual = getPathForRoot(path, root);
-        Path expected = Paths.get("C:\\custom-root\\ets\\photos.txt");
-        assertEquals(expected, actual);
+        try (FileSystem fileSystem = Jimfs.newFileSystem(Configuration.windows())) {
+            Path path = fileSystem.getPath("ets\\photos.txt");
+            Path root = fileSystem.getPath("C:\\custom-root");
+            Path actual = getPathForRoot(path, root);
+            Path expected = fileSystem.getPath("C:\\custom-root\\ets\\photos.txt");
+            assertEquals(expected, actual);
+        } catch (IOException e) {
+            fail();
+        }
     }
 
     @Test
     public void givenPath_whenRootIsNotAbsolute_thenThrow() {
-        Path path = Paths.get("ets\\photos.txt");
-        Path root = Paths.get("custom-root");
-        assertThrows(IllegalArgumentException.class, () -> getPathForRoot(path, root));
+        try (FileSystem fileSystem = Jimfs.newFileSystem(Configuration.windows())) {
+            Path path = fileSystem.getPath("ets\\photos.txt");
+            Path root = fileSystem.getPath("custom-root");
+            assertThrows(IllegalArgumentException.class, () -> getPathForRoot(path, root));
+        } catch (IOException e) {
+            fail();
+        }
     }
 }
