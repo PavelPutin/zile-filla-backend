b3cba1deae0175fd957f11277b3b1608607fe9ce
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-07-11 10:28:04 +0300

    chore: migrate gradle from groovy to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.3.1"
+    id("io.spring.dependency-management") version "1.1.5"
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
+}
+
+group = "ru.vsu.pavel"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+ext {
+    set("snippetsDir", file("build/generated-snippets"))
+    set("springModulithVersion", "1.2.1")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("com.google.jimfs:jimfs:1.3.0")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "zile-filla-backend"
